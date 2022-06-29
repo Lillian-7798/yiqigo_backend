55b08ea62f5842b676c5b2a1b6259fc9467c4c3f
Yuting Shen <1933037881@qq.com>
2022-06-30 00:31:01 +0800

    feat: Added the APIs of GroupBuyController and MessageController

--- YQGo/src/main/java/com/example/yqg_backend/controller/GroupBuyController.java
+++ YQGo/src/main/java/com/example/yqg_backend/controller/GroupBuyController.java
@@ -1,16 +1,77 @@
 package com.example.yqg_backend.controller;
 
 import com.example.yqg_backend.entity.Groupbuy;
+import com.example.yqg_backend.entity.Orders;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.util.List;
 
 @RestController
 public class GroupBuyController {
 
     @RequestMapping("/getGroupBuysByUser")
-    public Groupbuy getUser(@RequestParam("userid") Integer userid)
+    public List<Groupbuy> getUser(@RequestParam("userid") Integer userid)
     {
+        return null;
+    }
+
+    @RequestMapping(value = "/getGroupBuyDetail",method = RequestMethod.GET)
+    public Groupbuy getGroupBuyDetail(@RequestParam("groupBuyId") Integer groupBuyId){
+        return null;
+    }
+
+    @RequestMapping(value = "/addGroupBuy",method = RequestMethod.POST)
+    public boolean addGroupBuy(@RequestParam("title") String title,
+                               @RequestParam("description") String description,
+                               @RequestParam("logisticsType") Integer logisticsType,
+                               @RequestParam("startTime") Date startTime,
+                               @RequestParam("endTime") Date endTime,
+                               @RequestParam("isSecKill") Boolean isSecKill){
+        return true;
+    }
+
+    /* Not Sure Yet */
+    @RequestMapping(value = "/updateGroupBuy",method = RequestMethod.POST)
+    public boolean updateGroupBuy(@RequestParam("title") String title,
+                                  @RequestParam("description") String description,
+                                  @RequestParam("logisticsType") Integer logisticsType,
+                                  @RequestParam("startTime") Date startTime,
+                                  @RequestParam("endTime") Date endTime,
+                                  @RequestParam("isSecKill") Boolean isSecKill){
+        return true;
+    }
+
+    @RequestMapping(value = "/deleteGroupBuy")
+    public boolean deleteGroupBuy(@RequestParam("groupBuyId") Integer groupBuyId) {
+        return true;
+    }
+
+    @RequestMapping("endGroupBuy")
+    public boolean endGroupBuy(@RequestParam("groupBuyId") Integer groupBuyId) {
+        return true;
+    }
+
+    @RequestMapping(value = "/updateGroupBuyInventory")
+    public boolean updateGroupBuyInventory(@RequestParam("groupBuyId") Integer groupBuyId) {
+        return true;
+    }
+
+    @RequestMapping(value = "/getGroupBuyByLeader",method = RequestMethod.GET)
+    public List<Orders> getGroupBuyByLeader(@RequestParam("userId") Integer userId){
+        return null;
+    }
+
+    @RequestMapping(value = "/getGroupBuyByGoodsName",method = RequestMethod.GET)
+    public List<Orders> getGroupBuyByGoodsName(@RequestParam("name") String name){
+        return null;
+    }
+
+    @RequestMapping(value = "/getGroupBuyStatistics",method = RequestMethod.GET)
+    public List<Orders> getGroupBuyStatistics(@RequestParam("groupBuyId") Integer groupBuyId){
         return null;
     }
 }
--- YQGo/src/main/java/com/example/yqg_backend/controller/MessageController.java
+++ YQGo/src/main/java/com/example/yqg_backend/controller/MessageController.java
@@ -1,7 +1,33 @@
 package com.example.yqg_backend.controller;
 
+import com.example.yqg_backend.entity.Dialog;
+import com.example.yqg_backend.entity.Orders;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MessageController {
+    @RequestMapping(value = "/getDialogsByUser",method = RequestMethod.GET)
+    public List<Dialog> getDialogsByUser(@RequestParam("userID") Integer userID){
+        return null;
+    }
+
+    @RequestMapping(value = "/getDialog",method = RequestMethod.GET)
+    public Dialog getDialog(@RequestParam("userId1") Integer userId1, @RequestParam("userId2") Integer userId2){
+        return null;
+    }
+
+    @RequestMapping(value = "/addDialog",method = RequestMethod.POST)
+    public boolean addDialog(@RequestParam("userId1") Integer userId1,@RequestParam("userId2") Integer userId2){
+        return true;
+    }
+
+    @RequestMapping(value = "/addMessage",method = RequestMethod.POST)
+    public boolean addMessage(@RequestParam("dialogId") Integer dialogId,@RequestParam("userId") Integer userId, @RequestParam("content") String content){
+        return true;
+    }
 }
