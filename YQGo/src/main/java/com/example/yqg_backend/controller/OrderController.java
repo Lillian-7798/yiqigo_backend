86c557b37385f6191f9d54c1e35533c21b448c6b
lyx <lillian-7798@sjtu.edu.cn>
2022-06-29 18:24:44 +0800

    feat: add order and user controller.

--- YQGo/src/main/java/com/example/yqg_backend/controller/OrderController.java
+++ YQGo/src/main/java/com/example/yqg_backend/controller/OrderController.java
@@ -1,7 +1,44 @@
 package com.example.yqg_backend.controller;
 
+import com.example.yqg_backend.entity.Orders;
+import io.swagger.models.auth.In;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
+
+    @RequestMapping(value = "/getOrderByUser",method = RequestMethod.GET)
+    public Orders getOrderByUser(@RequestParam("userID") Integer userID){
+        return null;
+    }
+
+    @RequestMapping(value = "/getOrderDetail",method = RequestMethod.GET)
+    public Orders getOrderDetail(@RequestParam("orderID") Integer orderID){
+        return null;
+    }
+
+//    addOrder这里不知道前端的商品列表要怎么传进来
+    @RequestMapping(value = "/addOrder",method = RequestMethod.POST)
+    public boolean addOrder(@RequestParam("goodsList") List<Object> goods,
+                            @RequestParam("logisticsType") Integer logis,
+                            @RequestParam("diliverAddr") String diliverAddr,
+                            @RequestParam("receiveAddr") String receiveAddr,
+                            @RequestParam("GroupBuyID") Integer GBID){
+        return true;
+    }
+
+    @RequestMapping(value = "/getOrdersByGroupBuy",method = RequestMethod.GET)
+    public List<Orders> getOrdersByGroupBuy(@RequestParam("GroupBuyID") Integer GBID){
+        return null;
+    }
+
+    @RequestMapping(value = "/updateOrderStatus",method = RequestMethod.POST)
+    public boolean updateOrderStatus(@RequestParam("status") String status){
+        return true;
+    }
 }
--- YQGo/src/main/java/com/example/yqg_backend/controller/UserController.java
+++ YQGo/src/main/java/com/example/yqg_backend/controller/UserController.java
@@ -2,15 +2,31 @@ package com.example.yqg_backend.controller;
 
 import com.example.yqg_backend.entity.Users;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @RequestMapping("/getUser")
-    public Users getUser(@RequestParam("userid") Integer userid)
+    @RequestMapping(value = "/getUser",method = RequestMethod.GET)
+    public Users getUser(@RequestParam("userID") Integer userID)
     {
         return null;
+    }
+
+    @RequestMapping("/checkUser")
+    public boolean checkUser(@RequestParam("userName") String userName,@RequestParam("password") String password){
+        return true;
+    }
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    public boolean addUser(@RequestParam("userName") String userName,@RequestParam("password") String password){
+        return true;
+    }
+
+    @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
+    public boolean updateUseer(@RequestParam("userID") String userID,@RequestParam("address") String address){
+        return true;
     }
 }
