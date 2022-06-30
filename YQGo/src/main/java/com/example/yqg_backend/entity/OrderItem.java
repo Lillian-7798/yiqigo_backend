ee5930e588e25985368ff1e47cc8428ef9b977cf
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-06-30 11:31:15 +0800

    更新 OrderItem.java feat：完善orderitem实体类

--- YQGo/src/main/java/com/example/yqg_backend/entity/OrderItem.java
+++ YQGo/src/main/java/com/example/yqg_backend/entity/OrderItem.java
@@ -1,4 +1,56 @@
 package com.example.yqg_backend.entity;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class OrderItem {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "userId")
+    private int userId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "orderId")
+    private int orderId;
+    @Basic
+    @Column(name = "count")
+    private Integer count;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return userId == orderItem.userId && orderId == orderItem.orderId && Objects.equals(count, orderItem.count);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, orderId, count);
+    }
 }
