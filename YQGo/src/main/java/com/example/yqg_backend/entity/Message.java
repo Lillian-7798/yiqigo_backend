bca56afcef53e3bffcf5035f922eaa63108e7a16
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-06-30 11:30:45 +0800

    更新 Message.java feat：完善message实体类

--- YQGo/src/main/java/com/example/yqg_backend/entity/Message.java
+++ YQGo/src/main/java/com/example/yqg_backend/entity/Message.java
@@ -1,4 +1,66 @@
 package com.example.yqg_backend.entity;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Message {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "messageId")
+    private int messageId;
+    @Basic
+    @Column(name = "dialogId")
+    private Integer dialogId;
+    @Basic
+    @Column(name = "content")
+    private String content;
+    @Basic
+    @Column(name = "userId")
+    private Integer userId;
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public Integer getDialogId() {
+        return dialogId;
+    }
+
+    public void setDialogId(Integer dialogId) {
+        this.dialogId = dialogId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return messageId == message.messageId && Objects.equals(dialogId, message.dialogId) && Objects.equals(content, message.content) && Objects.equals(userId, message.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(messageId, dialogId, content, userId);
+    }
 }
