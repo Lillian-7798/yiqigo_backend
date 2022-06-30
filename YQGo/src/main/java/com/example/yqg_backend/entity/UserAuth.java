ece13c91a2b0ed743a38ac230fa279a37c82faf8
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-06-30 11:32:24 +0800

    更新 UserAuth.java feat：完善userauth实体类

--- YQGo/src/main/java/com/example/yqg_backend/entity/UserAuth.java
+++ YQGo/src/main/java/com/example/yqg_backend/entity/UserAuth.java
@@ -1,4 +1,55 @@
 package com.example.yqg_backend.entity;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class UserAuth {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "userId")
+    private int userId;
+    @Basic
+    @Column(name = "username")
+    private String username;
+    @Basic
+    @Column(name = "password")
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserAuth userAuth = (UserAuth) o;
+        return userId == userAuth.userId && Objects.equals(username, userAuth.username) && Objects.equals(password, userAuth.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, password);
+    }
 }
