47e28b29341ac793d697b57dc0413d12fdd18364
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-07-05 21:46:02 +0800

    更新 OrderRepository.java feat：添加了getOrderByUser和getOrderDetail两个方法

--- YQGo/src/main/java/com/example/yqg_backend/repository/OrderRepository.java
+++ YQGo/src/main/java/com/example/yqg_backend/repository/OrderRepository.java
@@ -7,4 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Query(value = "SELECT MAX(o.number) from orders o where group_buy_id= ?1", nativeQuery = true)
     Integer getMaxNumber(Integer groupBuyId);
+
+    @Query(value="select o from Order o where o.user.id=:userID")
+    List<Order> getOrderByUser(Integer userID);
+
+    @Query("select o from Order o where o.id=:orderID")
+    Order getOrderDetail(Integer orderID);
 }
