9aff996c89f4a122dbf0679821ccd024692c6a80
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-07-07 15:32:42 +0800

    feat:引入list和map

--- YQGo/src/main/java/com/example/yqg_backend/dao/OrderDao.java
+++ YQGo/src/main/java/com/example/yqg_backend/dao/OrderDao.java
@@ -2,6 +2,9 @@ package com.example.yqg_backend.dao;
 
 import com.example.yqg_backend.entity.Order;
 
+import java.util.List;
+import java.util.Map;
+
 public interface OrderDao {
     void addOrder(Order order);
     Integer getMaxNumber(Integer groupBuyId);
