4faabe83f6de1b6ace87dcc7fc7f2feb40ff2499
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-07-07 14:01:40 +0800

    feat：修改了getOrderByUser和getOrderDetail两个方法的数据类型

--- YQGo/src/main/java/com/example/yqg_backend/serviceimpl/OrderServiceImpl.java
+++ YQGo/src/main/java/com/example/yqg_backend/serviceimpl/OrderServiceImpl.java
@@ -1,73 +1,27 @@
 package com.example.yqg_backend.serviceimpl;
 
-import com.example.yqg_backend.dao.*;
-import com.example.yqg_backend.entity.*;
+import com.example.yqg_backend.dao.OrderDao;
+import com.example.yqg_backend.entity.Order;
 import com.example.yqg_backend.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
     @Autowired
     OrderDao orderDao;
-    @Autowired
-    OrderItemDao orderItemDao;
-    @Autowired
-    UserDao userDao;
-    @Autowired
-    GroupBuyDao groupBuyDao;
-    @Autowired
-    GoodDao goodDao;
 
     @Override
-    public void addOrder(RequestOrder requestOrder) {        // 缺乏库存检查
-        Order order = new Order(requestOrder.getLogisticsType(),
-                requestOrder.getDeliverAddr(),
-                requestOrder.getReceiveAddr(),
-                requestOrder.getNote());
-        order.setUser(userDao.getUser(requestOrder.getUserId()));
-        order.setGroupBuy(groupBuyDao.getGroupBuy(requestOrder.getGroupBuyId()));
-        order.setTime(new Timestamp(System.currentTimeMillis()));
-        order.setStatus(1);         // 暂时设状态为1
-        Integer maxNumber = orderDao.getMaxNumber(requestOrder.getGroupBuyId());
-        if (maxNumber == null) {
-            order.setNumber(1);
-        }
-        else {
-            order.setNumber(maxNumber + 1);
-        }
-
-        int total_count = 0;
-        int total_price = 0;
-        List<Orderitem> orderitems = new ArrayList<>();
-        for (RequestGoods reqGood : requestOrder.getGoods()) {
-            total_count += reqGood.getCount();
-            Good good = goodDao.getGood(reqGood.getGoodsId());
-            total_price += good.getPrice() * reqGood.getCount();
-            orderitems.add(new Orderitem(good, reqGood.getCount()));
-        }
-        order.setCount(total_count);
-        order.setPrice(total_price);
-        orderDao.addOrder(order);
-
-        for (Orderitem item : orderitems) {
-            item.setId(new OrderitemId(order.getId(), item.getGoods().getId()));
-            item.setOrder(order);
-        }
-        orderItemDao.addOrderItems(orderitems);
-    }
-
-    @Override
-    public List<Order> getOrderByUser(Integer userID){
+    public List<Map<String, Object>> getOrderByUser(Integer userID){
       return orderDao.getOrderByUser(userID);
     }
 
     @Override
-    public Order getOrderDetail(Integer orderID){
+    public Map<String,Object> getOrderDetail(Integer orderID){
       return orderDao.getOrderDetail(orderID);
     }
 }
