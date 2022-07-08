ecaa30cd9b463dabdea8e3367cadd5bfb61ed4e3
wty <cdb0b62c9cf7478fbaa5f357e3eb0636@example.com>
2022-07-08 14:40:01 +0800

    feat:编写了getUser和checkUser方法，增加了getUserByName方法

--- YQGo/src/main/java/com/example/yqg_backend/controller/UserController.java
+++ YQGo/src/main/java/com/example/yqg_backend/controller/UserController.java
@@ -1,30 +1,43 @@
 package com.example.yqg_backend.controller;
 
 import com.example.yqg_backend.entity.User;
+import com.example.yqg_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @CrossOrigin
 @RestController
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/getUser",method = RequestMethod.GET)
-    public User getUser(@RequestParam("userID") Integer userID)
+    public Map<String,Object> getUser(@RequestParam("userID") Integer userID)
     {
-        return null;
+        return userService.getUser(userID);
     }
 
-    @RequestMapping("/checkUser")
+    @RequestMapping(value="/checkUser",method=RequestMethod.GET)
     public boolean checkUser(@RequestParam("userName") String userName,@RequestParam("password") String password){
-        return true;
+        return userService.checkUser(userName,password);
+    }
+
+    @RequestMapping(value = "/getUserByName",method = RequestMethod.GET)
+    public Map<String,Object> getUserByName(@RequestParam("userName") String userName)
+    {
+        return userService.getUserByName(userName);
     }
 
     @RequestMapping(value = "/addUser",method = RequestMethod.POST)
-    public boolean addUser(@RequestParam("userName") String userName,@RequestParam("password") String password){
-        return true;
+    public boolean addUser(@RequestBody Map map){
+        return userService.addUser((String) map.get("userName"),(String) map.get("password"));
     }
 
     @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
-    public boolean updateUser(@RequestParam("userID") String userID,@RequestParam("address") String address){
+    public boolean updateUseer(@RequestParam("userID") String userID,@RequestParam("address") String address){
         return true;
     }
 }
